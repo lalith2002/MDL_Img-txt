47986ad2d8f21f77dcfee69ddeea4e8ec18552c5
Hamdancs <139947270+Hamdancs@users.noreply.github.com>
2023-11-28 15:20:35 +0530

    newcode.py

--- newcode.py
+++ newcode.py
@@ -1,1 +1,54 @@
+import easyocr as ocr  #OCR
+import streamlit as st  #Web App
+from PIL import Image #Image Processing
+import numpy as np #Image Processing 
+import pytesseract
+import shutil
 
+
+pytesseract.pytesseract.tesseract_cmd = None
+
+# search for tesseract binary in path
+@st.cache_resource
+def find_tesseract_binary() -> str:
+    return shutil.which("tesseract")
+
+# set tesseract binary path
+pytesseract.pytesseract.tesseract_cmd = find_tesseract_binary()
+if not pytesseract.pytesseract.tesseract_cmd:
+    st.error("Tesseract binary not found in PATH. Please install Tesseract.")
+#title
+st.title("Extracting texts from Image")
+
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg','pdf'])
+
+
+@st.cache
+def load_model(): 
+    reader = ocr.Reader(['en'],model_storage_directory='.')
+    return reader 
+
+reader = load_model() #load model
+
+if image is not None:
+
+    input_image = Image.open(image) #read image
+    st.image(input_image) #display image
+
+    with st.spinner("🤖 AI is at Work! "):
+        
+
+        result = reader.readtext(np.array(input_image))
+
+        result_text = [] #empty list for results
+
+
+        for text in result:
+            result_text.append(text[1])
+
+        st.write(result_text)
+    #st.success("Here you go!")
+    st.balloons()
+else:
+    st.write("Upload an Image")
